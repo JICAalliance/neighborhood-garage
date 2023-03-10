a772779550a3f02d3e5bc3b8911e83f43e083398
iwirsing <ivymolina@gmail.com>
2023-03-09 20:08:47 -0600

    user and index model

---
+++ server/models/index.js
@@ -0,0 +1,7 @@
+const User = require('./User');
+const Garage = require('./Garage');
+const Tool = require('./Tool');
+const Checkout = require('./Checkout');
+const Message = require('./Message');
+
+module.exports = { User, Garage, Tool, Checkout, Message };
