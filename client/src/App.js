031757fda265482c3d04287436842ebffdaf0422
ckim812 <109961170+ckim812@users.noreply.github.com>
2023-03-12 15:00:17 -0500

    worked on routing and auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
+<<<<<<< Updated upstream
 import "./App.scss";
 import { Nav, Home, SignUp, LogIn, Profile } from "./components";
 import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./components/auth";
+=======
+import "./App.css";
+import { Nav, Home, Signup, Login, Profile } from "./components";
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import { AuthProvider } from "./components/utils/auth";
+>>>>>>> Stashed changes
 
 function App() {
   const [page, setPage] = useState("home");
@@ -10,6 +18,7 @@ function App() {
   // return <Nav setPage={setPage} loggedIn={setLogin} />;
 
   return (
+<<<<<<< Updated upstream
     <>
       <AuthProvider>
         <Nav setPage={setPage} loggedIn={setLogIn} />
@@ -21,6 +30,17 @@ function App() {
         </Routes>
       </AuthProvider>
     </>
+=======
+    <AuthProvider>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </AuthProvider>
+>>>>>>> Stashed changes
   );
 }
 
--- client/src/components/logIn/logIn.js
+++ client/src/components/logIn/logIn.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../auth";
@@ -22,5 +23,33 @@ const LogIn = () => {
     </div>
   );
 };
+=======
+import "./login.scss";
+import { useState } from "react";
+import { useAuth } from "../utils/auth";
+import { useNavigate } from "react-router-dom";
 
-export default LogIn;
+const Login = () => {
+  const [user, setUser] = useState("");
+  const auth = useAuth();
+  const navigate = useNavigate();
+>>>>>>> Stashed changes
+
+  const handleLogin = () => {
+    auth.login(user);
+    navigate("/");
+  };
+
+  return (
+    <div id="login">
+      <h1>This is the login page</h1>
+      <label>
+        Username:{" "}
+        <input type="text" onChange={(e) => setUser(e.target.value)} />
+      </label>
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+export default Login;
