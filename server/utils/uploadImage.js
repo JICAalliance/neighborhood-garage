e11c65f18601c27ddadc2524179fe0b63932adad
aknoedler <andrewknoedler89@gmail.com>
2023-03-13 20:04:51 -0500

    created addtool page

---
+++ server/utils/uploadImage.js
@@ -0,0 +1,26 @@
+const cloudinary = require('cloudinary');
+const path = require('path');
+require('dotenv').config({path: path.resolve(__dirname, "../.env")});
+
+const cloudName = process.env.CLOUDINARY_CLOUD_NAME;
+const apiKey = process.env.CLOUDINARY_API_KEY;
+const apiSecret = process.env.CLOUDINARY_API_SECRET;
+
+cloudinary.config({
+    cloud_name: cloudName,
+    api_key: apiKey,
+    api_secret: apiSecret
+});
+
+const opts = {
+    overwrite: true,
+    invalidate: true,
+    resource_type: 'auto'
+}
+
+const uploadImage = (image) => {
+    const submit = cloudinary.uploader.upload(image, opts);
+    return submit;
+}
+
+module.exports = uploadImage;
