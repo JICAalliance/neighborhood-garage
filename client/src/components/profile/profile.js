145174e8758707a9dbc16e55ba5d046158fe9496
Jazmin Tweedle <jazmin.tweedle@gmail.com>
2023-03-14 17:35:10 -0500

    added links to dropdown to navigate to garages

--- client/src/components/profile/profile.js
+++ client/src/components/profile/profile.js
@@ -6,7 +6,10 @@ const Profile = () => {
   const navigate = useNavigate()
   const navToProduct = (e) => {
     // navigate(e.target.value)
-    return(navigate("/login"))
+    if (e.target.value !== "Choose Garage") {
+      return(navigate(`/${e.target.value}`))
+    }
+    // console.log(e.target.value)
   }
 
   return(
@@ -15,11 +18,11 @@ const Profile = () => {
     <div><button>Create Garage</button></div>
     <div><button>Join Garage</button></div>
     {/* product will be what is displayed, index for unique ID; replace the array with dynamic values of garages user is in*/}
-      <select name="product">
+      <select name="product" onClick={(e) => navToProduct(e)}>
         <option>Choose Garage</option>
         {["Garage 1", "Garage 2", "Garage 3"].map((product, index) => {
           return(
-            <option value={product} key={index} onClick={() => navToProduct()}>{product}</option>
+            <option value={product} key={index} onClick={(e) => navToProduct(e)}>{product}</option>
           )
         })}
       </select>
