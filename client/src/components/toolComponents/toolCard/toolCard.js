17c59626e1361c202ce3d4bfe973447356f75db2
iwirsing <ivymolina@gmail.com>
2023-03-17 13:25:03 -0500

    edited the navigation and responses of webpage after create garage and join garage and leave garage

--- client/src/components/toolComponents/toolCard/toolCard.js
+++ client/src/components/toolComponents/toolCard/toolCard.js
@@ -1,51 +1,3 @@
-// import './toolCard.scss';
-// import React from "react";
-
-// import { Card, Icon, Button, Image } from 'semantic-ui-react';
-// import ToolCheckout from '../toolCheckout';
-
-
-// const ToolCard = ({ tool, checkoutModal }) => {
-
-//   const [borrowed, setBorrowed] = React.useState(Boolean(tool.checkout));
-
-//   return <div id='tool-container'>
-//     <Card key={tool._id} id={tool._id}>
-//       <Card.Content>
-//         <Image
-//           // floated='right'
-//           // size='mini'
-//           src={tool.image}
-//         />
-//         <Card.Header>{tool.name}</Card.Header>
-//         <Card.Meta>
-//           {tool.checkout ?
-//             <div>
-//               Status: Borrowed
-//             </div>
-//             :
-//             <div>
-//               Status: Available
-//             </div>}
-//         </Card.Meta>
-//         <Card.Description>
-//           {tool.description}
-//         </Card.Description>
-//       </Card.Content>
-
-//       {checkoutModal ?
-
-//         <Card.Content extra>
-//           <ToolCheckout _id={tool._id} name={tool.name} description={tool.description} image={tool.image} checkout={tool.checkout} setBorrowed={setBorrowed} />
-//         </Card.Content>
-//         : ''}
-//     </Card>
-//   </div>
-
-// };
-
-
-// export default ToolCard;
 import './toolCard.scss';
 import React from "react";
 
@@ -56,8 +8,6 @@ import { QUERY_CHECKOUT_BORROWER } from '../../utils/queries';
 
 
 const ToolCard = ({ tool, checkoutModal }) => {
-
-
 
   const [borrowed, setBorrowed] = React.useState(Boolean(tool.checkout));
   let checkoutId = null;
@@ -98,14 +48,13 @@ const ToolCard = ({ tool, checkoutModal }) => {
         </Card.Description>
       </Card.Content>
 
-        {checkoutModal ?
+      {checkoutModal ?
 
-          <Card.Content extra>
-            <ToolCheckout _id={_id} name={name} description={description} image={image} checkout={checkout} />
-          </Card.Content>
-          : ''}
-      </Card>
-
+        <Card.Content extra>
+          <ToolCheckout _id={tool._id} name={tool.name} description={tool.description} image={tool.image} checkout={tool.checkout} setBorrowed={setBorrowed} />
+        </Card.Content>
+        : ''}
+    </Card>
   </div>
 
 };
