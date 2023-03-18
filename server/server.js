4c9a7c55de3b764b809892384c173e8f76579576
ckim812 <109961170+ckim812@users.noreply.github.com>
2023-03-18 16:08:42 -0500

    added test stripe components

--- server/server.js
+++ server/server.js
@@ -14,6 +14,44 @@ const server = new ApolloServer({
     context: authMiddleware,
 });
 
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY, {
+    apiVersion: "2022-08-01",
+  });
+  
+  app.use(express.static(process.env.STATIC_DIR));
+  
+  app.get("/", (req, res) => {
+    const path = resolve(process.env.STATIC_DIR + "/index.html");
+    res.sendFile(path);
+  });
+  
+  app.get("/config", (req, res) => {
+    res.send({
+      publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+    });
+  });
+  
+  app.post("/create-payment-intent", async (req, res) => {
+    try {
+      const paymentIntent = await stripe.paymentIntents.create({
+        currency: "EUR",
+        amount: 1999,
+        automatic_payment_methods: { enabled: true },
+      });
+  
+      // Send publishable key and PaymentIntent details to client
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    } catch (e) {
+      return res.status(400).send({
+        error: {
+          message: e.message,
+        },
+      });
+    }
+  });
+
 app.use(express.urlencoded({ limit: "50mb", extended: false }));
 app.use(express.json({ limit: "50mb" }));
 
