5743e6ae416983494672c600a7be41a34bd06f96
ckim812 <109961170+ckim812@users.noreply.github.com>
2023-03-19 08:11:36 -0500

    added links back to profile and cart on success and cancel pages

--- client/src/components/pages/Cancel.js
+++ client/src/components/pages/Cancel.js
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+
 function Cancel() {
-  return <h1>Sorry to see you cancelled your Stripe payment!</h1>;
+  return (
+    <>
+      <h1>Sorry to see you cancelled your Stripe payment!</h1>
+      <Link reloadDocument to="/store">
+        ← Back to Cart
+      </Link>
+    </>
+  );
 }
 
 export default Cancel;
